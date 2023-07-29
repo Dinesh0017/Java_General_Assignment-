c10e7894c0d4b56db658a25b055e143c5e92bacc
Dinesh Rathnasiri <131970897+Dinesh0017@users.noreply.github.com>
2023-07-29 08:30:18 +0530

    Add files via upload

---
+++ Question14 GUI/Question14.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.question14;
+
+public class Question14 {
+
+    public static void main(String[] args) {
+        Convertor c = new Convertor();
+        c.show();
+    }
+}
